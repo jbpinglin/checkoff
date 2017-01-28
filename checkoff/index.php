9f6a5e9a2cf0fbc35f84f1da565196b42d0416b1
jbbrassoud <jbbrassoud@gmail.com>
2017-01-28 16:43:18 +0100

    New index

--- checkoff/index.php
+++ checkoff/index.php
@@ -1,16 +1,94 @@
 <html>
-    <head>    
-    
-    
+    <head>
+        <meta title="ChekOff">
+        <link rel="stylesheet" href="stl.css">
     </head>
-
     <body>
+        <?php include('header.php'); ?>
+        <header>
+            <a><img src="img/logow.png"></a>
+        </header>
+        <div class="content">
+            <div class="pad"></div>
+            <div class="index" >
+                    <table>
+                        <tr>
+                            <td>
+                                <a href="parapente.php"><div class="center" style=" background-image: url(img/1top.jpeg);  ">    
+                                </div></a>
+                            </td>
+                            <td>
+                                <a href="parapente.php"><div class="center" style=" background-image: url(img/jogging/imgjhead.jpg);  ">
+                                </div>
+                                </a>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey; ">
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey; ">
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey; ">
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey;  ">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="center" style=" background-color:grey; ">
+                                </div>
+                            </td>
+                        </tr>
+                        
+                    </table>
+                </div>
         
-        <a href="parapente.php">
-        
-        </a>
-    
+        </div>
     </body>
-    
-    
-</html>
+</html> 
