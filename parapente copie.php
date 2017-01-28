4505dea8e72396e99b8b3ed6a54af90f7a20b949
jbbrassoud <jbbrassoud@gmail.com>
2017-01-28 17:04:12 +0100

    jbb

---
+++ parapente copie.php
@@ -0,0 +1,210 @@
+<html>
+    <head>
+        
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="stl.css">  
+        <title>I belive I can fly</title>
+
+    </head>
+    
+    <body>
+        
+        <h1> I believe I can fly</h1>
+
+        
+<div class="pad"></div>
+    <div class="content">
+        
+                <div class="top" >
+                    <table>
+                        <tr>
+                            <td>
+                                <div class="left" style=" background-image: url(img/parapente/1top.jpeg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="center" style=" background-image: url(img/parapente/1top.jpeg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="right" style=" background-image: url(img/parapente/1top.jpeg);  ">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                
+                <div class="bot">
+                    <table>
+                       <tr>
+                            <td>
+                                <div class="left" style=" background-image: url(img/parapente/1bot.jpeg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="center" style=" background-image: url(img/parapente/1bot.jpeg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="right" style=" background-image: url(img/parapente/1bot.jpeg);  ">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+        
+        <p> La nouvelle drogue des quartiers chics se répend dans       Paris.</br>
+            Le hashish ne leur suffit plus, la recherche toujours plus extrême de sensation forte amène les bobos parisiens à tester une nouvelle drogue:</br>
+            Le Parapente.</br>
+            Un  loisir à haut risque qui procure les mêmes effets que les drogues dure : exitation, dilatation de la pupille, hillusion de puissance, sensation de planner hyper  réaliste   et une addiction pouvant être mortel.</p>
+        <p>Tout le monde s’y essaye,</br>
+            le rappeur Snoop Dogg aurait même déclaré :</p>
+
+                <div class="top" >
+                    <table>
+                        <tr>
+                            <td>
+                                <div class="left" style=" background-image: url(img/parapente/2top.jpg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="center" style=" background-image: url(img/parapente/2top.jpg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="right" style=" background-image: url(img/parapente/2top.jpg);  ">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                
+                <div class="bot">
+                    <table>
+                       <tr>
+                            <td>
+                                <div class="left" style=" background-image: url(img/parapente/2bot.jpg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="center" style=" background-image: url(img/parapente/2bot.jpg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="right" style=" background-image: url(img/parapente/2bot.jpg);  ">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            
+        <h3>«Ce truc c’est trop de la bombe, quand tu en fais, tu       décoles et tu planes pendant 3 heures»</h3>
+        <p> Pourtant, la pratique de cette activité n’est pas sans       risque, à trop forte dose, c’elle-ci entrainerait des       séquelles iréverssible.</br>
+            C’est le cas de Philippe, un bobo affectueux aillant perdu l’usage de ses membres lors d’un bad trip. </p>
+                <div class="top" >
+                    <table>
+                        <tr>
+                            <td>
+                                <div class="left" style=" background-image: url(img/parapente/3top.png);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="center" style=" background-image: url(img/parapente/3top.png);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="right" style=" background-image: url(img/parapente/3top.png);  ">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                
+                <div class="bot">
+                    <table>
+                       <tr>
+                            <td>
+                                <div class="left" style=" background-image: url(img/parapente/3bot.png);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="center" style=" background-image: url(img/parapente/3bot.png);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="right" style=" background-image: url(img/parapente/3bot.png);  ">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+        <h3>«Avec le parapente j’avais tout, je prenais de la hauteur, je voyais les choses d’en haut puis je souflais, j’avais pour idée que l’on pissais sur le monde».</h3>
+        <p> Les dangers de ce loisir ne semble pas pour autant inquiéter la justice française.</br>
+            C’est donc en toute légalité que de plus en plus de clubs de parapente voient le jour dans la capitale.</br>
+            Encadré par des consomateurs professionnel, les clients pourrons se planifier des séances de haute voltiges dans un cadre sécurisé.</p>
+        <p> Nous avertissons nos lecteurs sur les dangers de cette activité, il est fortement déconseillé de reproduire ce genre d’experiences avec du materiel inaproprié comme
+            le soutien gorge de votre amie.</br>
+            Pour votre sécurité, faite le encadré par des professionnels.</p>
+                
+                <div class="top" >
+                    <table>
+                        <tr>
+                            <td>
+                                <div class="left" style=" background-image: url(img/parapente/4top.jpg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="center" style=" background-image: url(img/parapente/4top.jpg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="right" style=" background-image: url(img/parapente/4top.jpg);  ">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                
+                <div class="bot">
+                    <table>
+                       <tr>
+                            <td>
+                                <div class="left" style=" background-image: url(img/parapente/4bot.jpg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="center" style=" background-image: url(img/parapente/4bot.jpg);  ">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="right" style=" background-image: url(img/parapente/4bot.jpg);  ">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+        
+        <p> Tu peux consulter les clubs partenaires en cliquant
+            <a  href="">ICI</a></p>
+
+</div>
+
+    </body>
+</html>
